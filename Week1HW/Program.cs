b8cd2d30cf6468bae893a393ef7b0a74aacaa143
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-08-23 19:33:23 -0400

    Add project files.

---
+++ Week1HW/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Week1HW
+{
+    using static System.Console;
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Who is being seen today?");
+
+            string patientName = Console.ReadLine();
+
+            Console.WriteLine("Welcome " + patientName);
+        }
+    }
+}
