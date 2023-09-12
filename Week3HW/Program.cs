09049b33b6a43d0da7fc1c5ffc3fbbb8e85b5043
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-09-12 13:35:20 -0400

    Week3HW

---
+++ Week3HW/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using static System.Console;
+
+namespace Week3HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("Menu:");
+                Console.WriteLine("a. Doctor – Sick Appointment");
+                Console.WriteLine("b. Doctor – Check-up");
+                Console.WriteLine("c. Exit");
+                Console.Write("Enter your choice (a/b/c): ");
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "a":
+                        Console.Write("Is the patient a child or an adult (child/adult): ");
+                        string patientType = Console.ReadLine();
+                        decimal baseCharge = 0;
+
+                        if (patientType == "child")
+                        {
+                            baseCharge = 50;
+                        }
+                        else if (patientType == "adult")
+                        {
+                            baseCharge = 75;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter 'child' or 'adult'.");
+                            continue;
+                        }
+
+                        Console.Write("Did the user have labs done (yes/no): ");
+                        string labsDone = Console.ReadLine();
+
+                        if (labsDone == "yes")
+                        {
+                            baseCharge += 25;
+                        }
+
+                        Console.WriteLine($"Total charge for the appointment: {baseCharge:C}");
+                        break;
+
+                    case "b":
+                        Console.Write("Is the patient a child or an adult (child/adult): ");
+                        string patientTypeCheckup = Console.ReadLine();
+                        decimal checkupCharge = 0;
+
+                        if (patientTypeCheckup == "child")
+                        {
+                            checkupCharge = 75;
+                        }
+                        else if (patientTypeCheckup == "adult")
+                        {
+                            checkupCharge = 100;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input. Please enter 'child' or 'adult'.");
+                            continue;
+                        }
+
+                        Console.WriteLine($"Total charge for the check-up: {checkupCharge:C}");
+                        break;
+
+                    case "c":
+                        Console.WriteLine("Exiting program.");
+                        return;
+
+                    default:
+                        Console.WriteLine("Invalid choice. Please select 'a', 'b', or 'c'.");
+                        break;
+                }
+                ReadKey();
+            }
+        }
+    }
+}
---
+++ Week4Class/Program.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using static System.Console;
+
+namespace Week3Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 5;
+            int second = 3;
+
+            if (first == second)
+            {
+                WriteLine("First equals second");
+            }
+            else
+            {
+                if (first > second)
+                {
+                    WriteLine("First is greater than second");
+                }
+                else
+                {
+                    WriteLine("First is less than second");
+                }
+            }
+
+
+
+            char ch = 'x';
+
+            if (ch == 'A' || ch == 'a')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'E' || ch == 'e')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'I' || ch == 'i')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'O' || ch == 'o')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'U' || ch == 'u')
+            {
+                WriteLine("Vowel");
+            }
+            else
+            {
+                WriteLine("Consonant");
+            }
+
+
+
+            int option, a, b, total;
+
+            WriteLine("please enter your first number");
+            a = Convert.ToInt32(ReadLine());
+
+            WriteLine("please enter your second number");
+            b = Convert.ToInt32(ReadLine());
+
+
+            WriteLine("1 - add number, 2 - subtraction, 3 - multiply, 4 - divide");
+            option = Convert.ToInt32(ReadLine());
+
+            switch(option)
+            {
+                case 1:
+                    total = (a + b);
+                    WriteLine("your tatal is {0}", total);
+                    break;
+                case 2:
+                    total = (a - b);
+                    WriteLine("your tatal is {0}", total);
+                    break;
+                case 3:
+                    total = (a * b);
+                    WriteLine("your tatal is {0}", total);
+                    break;
+                case 4:
+                    total = (a / b);
+                    WriteLine("your tatal is {0}", total);
+                    break;
+                default:
+                    WriteLine("YOU DIDN'T SELECT 1-4");
+                    break;
+            }
+
+            ReadKey();
+
+        }
+    }
+}
