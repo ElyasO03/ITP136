7fbab35604528d6815433636cdf0b0d221468768
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-09-20 18:05:11 -0400

    fcost fixed

--- Week4HW/Program.cs
+++ Week4HW/Program.cs
@@ -12,7 +12,7 @@ namespace Week4HW
 
             food1.FoodID = 1;
             food1.fname = "Pizza";
-            food1.fdescription = "Pizza is a flat, open-faced baked pie that originated in Italy.";
+            food1.fdescription = "is a flat, open-faced baked pie that originated in Italy.";
             food1.fcost = 11.00;
 
             food2.FoodID = 2;
@@ -20,8 +20,8 @@ namespace Week4HW
             food2.fdescription = "a Mexican food that consists of a folded and usually fried piece of thin bread that is filled with meat, cheese, lettuce, etc.";
             food2.fcost = 5.00;
 
-            WriteLine("{0,5} {1,5} {2,10}", food1.FoodID, food1.fname, food1.fdescription, food1.fcost);
-            WriteLine("{0,5} {1,5} {2,10}", food2.FoodID, food2.fname, food2.fdescription, food2.fcost);
+            WriteLine("{0,5} {1,5} {2,10}, {3,10}", food1.FoodID, food1.fname, food1.fdescription, food1.fcost);
+            WriteLine("{0,5} {1,5} {2,10}, {3,10}", food2.FoodID, food2.fname, food2.fdescription, food2.fcost);
 
             ReadKey();
         }
