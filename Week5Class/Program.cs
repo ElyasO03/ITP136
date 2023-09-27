d4c5ae249acf1571d80b26b11f9853c156529872
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-09-27 11:59:09 -0400

    Week5HW

---
+++ Week5Class/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using static System.Console;
+
+namespace Week5Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+            
+            welcomeMessage();
+
+            for(int k= 0; k < 3; k ++)
+            {
+                a = askNumber();
+                b = askNumber();
+
+                addNumber(a, b);
+            }
+
+            ReadKey();
+
+        }
+
+        static void welcomeMessage()
+        {
+            WriteLine("Welcome to my method");
+        }
+
+        static int askNumber()
+        {
+            int x;
+            WriteLine("Enter a number");
+            x = Convert.ToInt32(ReadLine());
+
+            return x;
+        }
+
+        static void addNumber(int num1, int num2)
+        {
+            int total = num1 + num2;
+
+            WriteLine("Your tatal is {0}", total);
+        }
+    }
+}
---
+++ Week5HW/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using static System.Console;
+
+namespace Week5HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WelcomeStatement();
+
+            int secretNumber = 1;
+            MagicNumber(secretNumber);
+
+            int a, b;
+
+            for(int k=0; k < 1; k ++)
+            {
+                a = asknumber();
+                b = asknumber();
+
+                multiplynumber(a, b);
+            }
+
+            double taxRate = localTaxRate();
+            WriteLine("Your local tax rate is " + taxRate + "%");
+
+
+            ReadKey();
+        }
+
+    static void WelcomeStatement()
+        {
+            WriteLine("Welcome to My Method Examples");
+        }
+
+    static void MagicNumber(int secretNumber)
+        {
+            WriteLine("The magic number is " + secretNumber);
+        }
+
+    static int asknumber()
+        {
+            int x;
+            WriteLine("Enter a number");
+            x = Convert.ToInt32(ReadLine());
+
+            return x;
+        }
+
+    static void multiplynumber(int num1, int num2)
+        {
+            int total = num1 * num2;
+
+            WriteLine("Your total is {0}", total);
+        }
+
+    static double localTaxRate()
+        {
+            WriteLine("Please enter your local tax rate ");
+            string input = ReadLine();
+
+            return Convert.ToDouble(input);
+        }
+
+    }
+}
