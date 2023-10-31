da1ac8d94138c8af383de617c2e1186b5b824616
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-10-31 13:22:04 -0400

    Module 8 Assignment

---
+++ Week8HW/Parts.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace Week8HW
+{
+    class Parts
+    {
+        int partNum;
+        string partName, partDescription;
+        double cost;
+
+        public Parts(int partNum, string partName, string partDescription, double cost)
+        {
+            this.partNum = partNum;
+            this.partName = partName;
+            this.partDescription = partDescription;
+            this.cost = cost;
+        }
+
+        public int PartNum
+        {
+            get { return partNum; }
+            set { partNum = value; }
+        }
+
+        public string PartName
+        {
+            get { return partName; }
+            set { partName = value; }
+        }
+
+        public string PartDescription
+        {
+            get { return partDescription; }
+            set { partDescription = value; }
+        }
+
+        public double Cost
+        {
+            get { return cost; }
+            set { cost = value; }
+        }
+    }
+}
--- Week8HW/Program.cs
+++ Week8HW/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using static System.Console;
 
 namespace Week8HW
 {
@@ -9,6 +10,60 @@ namespace Week8HW
     {
         static void Main(string[] args)
         {
+            WriteLine("How many parts do you want to enter?");
+            int numParts = Convert.ToInt32(ReadLine());
+
+            Parts[] partsArray = new Parts[numParts];
+
+            for (int i = 0; i < partsArray.Length; i++)
+            {
+                partsArray[i] = PopulateParts();
+            }
+
+            WriteLine("Parts List:");
+            for (int i = 0; i < partsArray.Length; i++)
+            {
+                WriteLine($"{i + 1}. {partsArray[i].PartNum}, {partsArray[i].PartName}, {partsArray[i].PartDescription}, {partsArray[i].Cost}");
+            }
+
+            WriteLine("Which part do you want to view?");
+            int selectedPartIndex = Convert.ToInt32(ReadLine()) - 1;
+
+            if (selectedPartIndex >= 0 && selectedPartIndex < partsArray.Length)
+            {
+                DisplayPartDetails(partsArray[selectedPartIndex]);
+            }
+            else
+            {
+                WriteLine("Invalid selection.");
+            }
+        }
+
+        static Parts PopulateParts()
+        {
+            WriteLine("Please enter Part Number:");
+            int partNum = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter Part Name:");
+            string partName = ReadLine();
+
+            WriteLine("Please enter Part Description:");
+            string partDesc = ReadLine();
+
+            WriteLine("Please enter Cost:");
+            double cost = Convert.ToDouble(ReadLine());
+
+            return new Parts(partNum, partName, partDesc, cost);
+        }
+
+        static void DisplayPartDetails(Parts part)
+        {
+            WriteLine($"Part Number: {part.PartNum}");
+            WriteLine($"Part Name: {part.PartName}");
+            WriteLine($"Part Description: {part.PartDescription}");
+            WriteLine($"Cost: {part.Cost}");
+
+            ReadKey();
         }
     }
 }
