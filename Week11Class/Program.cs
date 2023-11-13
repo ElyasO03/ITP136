9994b13b28657b8e217434a8e033d7dc246619ea
Elyas Ozbek <109240976+ElyasO03@users.noreply.github.com>
2023-11-12 22:14:50 -0500

    Week11HW

---
+++ Week11Class/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+namespace Week11Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numberArray = { "Gretzky", "Jordan", "Pippin" };
+
+            try
+            {
+                WriteLine("Please enter the element you wish to see");
+
+                int elementNumber = Convert.ToInt32(ReadLine());
+
+
+                WriteLine(numberArray[elementNumber]);
+            }
+            catch(IndexOutOfRangeException e)
+            {
+                WriteLine("Don't enter a number larger than 2" + "\nERROR: " + e);
+            }
+
+
+
+
+
+
+
+
+            int num1, num2;
+
+            WriteLine("Please enter the first number");
+            num1 = Convert.ToInt32(ReadLine());
+
+            WriteLine("Please enter the second number");
+            num2 = Convert.ToInt32(ReadLine());
+
+            double total;
+
+            try
+            {
+                total = num1 / num2;
+                WriteLine("Your total is " + total);
+            }
+            catch(DivideByZeroException e)
+            {
+                WriteLine("Sorry you entered zero for your second number, don't to that" + "\nERROR: " + e);
+            }
+
+            ReadKey();
+
+        }
+    }
+}
---
+++ Week11HW/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using static System.Console;
+
+
+namespace Week11HW
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double num1, num2, result;
+
+                WriteLine("Please enter the first number");
+                num1 = Convert.ToDouble(ReadLine());
+
+                WriteLine("Please enter the second number");
+                num2 = Convert.ToDouble(ReadLine());
+
+                WriteLine("Choose a math operation:");
+                WriteLine("1 - Addition");
+                WriteLine("2 - Multiplication");
+                WriteLine("3 - Subtraction");
+                WriteLine("4 - Division");
+
+                int operation = Convert.ToInt32(ReadLine());
+
+                switch (operation)
+                {
+                    case 1:
+                        result = num1 + num2;
+                        break;
+                    case 2:
+                        result = num1 * num2;
+                        break;
+                    case 3:
+                        result = num1 - num2;
+                        break;
+                    case 4:
+                        try
+                        {
+                            result = num1 / num2;
+                        }
+                        catch (DivideByZeroException e)
+                        {
+                            WriteLine("Error: Division by zero is not allowed");
+                            WriteLine("ERROR: " + e);
+                            return;
+                        }
+                        break;
+                    default:
+                        WriteLine("Invalid operation selected");
+                        return;
+                }
+
+                WriteLine("Your result is: " + result);
+            }
+            catch (FormatException e)
+            {
+                WriteLine("Error: Invalid input. Please enter numeric values.");
+                WriteLine("ERROR: " + e);
+            }
+            catch (Exception e)
+            {
+                WriteLine("An unexpected error occurred: " + e);
+            }
+
+            ReadKey();
+        }
+    }
+}
